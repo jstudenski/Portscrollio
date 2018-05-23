c9c7393ddcf6202927e11c47b4c8a9e456e72a65
Jeff Studenski <jstudenski@gmail.com>
2018-05-22 20:01:05 -0500

    updated app

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import logo from '../images/logo.svg';
 import Hello from '../Hello';
+import Section from '../Section';
 import './App.css';
 
 
@@ -9,18 +10,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <header className="App-header">
           <h1 className="App-title">React Starter</h1>
-        </header>
-        <Router>
+        </header> */}
+        {/* <Router>
           <Route path="/hello" component={Hello} />
-        </Router>
-        <p className="App-intro">
-          Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        </Router> */}
+
+        <nav>
+          home
+          about
+          skills
+          portfolio
+          contact
+        </nav>
+
+
+        <Section background="black"></Section>
+        <Section background="red"><h1>Hello</h1></Section>
+        <Section background="orange"></Section>
+        <Section background="yellow"></Section>
       </div>
     );
   }
