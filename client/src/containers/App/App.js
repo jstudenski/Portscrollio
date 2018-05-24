50a0424723f3d2e5cb55b8a5805ad9bf0a7354ad
Jeff Studenski <jstudenski@gmail.com>
2018-05-24 12:48:54 -0500

    working scroll

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Section from '../Section';
 import Navagation from '../Navagation';
 import './App.css';
+import ScrollTo from 'react-scroll-into-view'
 
 {/* <nav>
 home
@@ -14,12 +15,33 @@ contact
 </nav> */}
 
 class App extends Component {
+
+//   componentDidMount() {
+//     console.log(this.refs["item"])
+//    // this.refs.scrollIntoView();    //this['item-1'].scrollIntoView();
+//  //  this.refs["item"].scrollIntoView({block: 'end', behavior: 'smooth'});
+
+//    //  this.refs[1].scrollIntoView({block: 'end', behavior: 'smooth'});
+//   }
+//   handleShow(i) {
+//     this.setState({index: i});
+//     console.log(i)
+//   }
+
   render() {
+
+    const id = `section${2}`;
     return (
       <div className="App">
+        <ScrollTo selector={`#section${3}`}>
+          TEST
+        </ScrollTo>
         <Section background="#FF5445"></Section>
         <Navagation/>
-        <Section background="#F0F0F0" ref="one"></Section>
+        <Section background="#F0F0F0">
+        <div class="ttt" id={`section${3}`}>
+        </div>
+        </Section>
         <Section background="#1C86EA"></Section>
         <Section background="#F9B887"></Section>
       </div>
