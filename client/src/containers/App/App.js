3b93f99e41eaa9d2d83144ad41a3bbd7879564ad
jstudenski <jstudenski@gmail.com>
2018-05-24 19:13:09 -0500

    colors

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Section from '../Section';
 import Navagation from '../Navagation';
+import Home from '../Home';
 import './App.css';
 
 class App extends Component {
@@ -9,12 +10,14 @@ class App extends Component {
     const id = `section${2}`;
     return (
       <div className="App">
-        <Section background="pink" name="home"></Section>
+        <Section background="#e0fff7" name="home">
+          <Home />
+        </Section>
         <Navagation/>
-        <Section background="#FF5445" name="about"></Section>
-        <Section background="#F0F0F0" name="skills"></Section>
-        <Section background="#1C86EA" name="portfolio"></Section>
-        <Section background="#F9B887" name="contact"></Section>
+        <Section background="#ffffff" name="about"></Section>
+        <Section background="#ffe8e0" name="skills"></Section>
+        <Section background="#D3FFB9" name="portfolio"></Section>
+        <Section background="#33373e" name="contact"></Section>
       </div>
     );
   }
--- client/src/containers/Section/Section.js
+++ client/src/containers/Section/Section.js
@@ -4,29 +4,10 @@ import API from '../../utils/API';
 import ScrollTo from 'react-scroll-into-view'
 
 class Section extends Component {
-  // state = {
-  //   getSamples: [{
-  //     "name":"hello"},{"name":"world"}],
-  // }
-
-  // componentDidMount() {
-  //   this.loadSample();
-  // }
-
-  // loadSample = () => {
-  //   API.getSamples()
-  //     .then(res => this.setState({ getSample: res.data }))
-  //     .catch(err => console.log(err));
-  // };
-
-  // <div className="ttt" id={`section${3}`}>
-  // </div>
-
   render() {
       return (
         <section style={{background : this.props.background}} id={this.props.name}>
           {this.props.children}
-          <p>{this.props.name}</p>
         </section>
       )
     }
