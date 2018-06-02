1ed616f84a32ded1cf8a5813be8ca72e988437d3
jstudenski <jstudenski@gmail.com>
2018-06-02 18:59:02 -0500

    written as pure component

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import './Home.css';
 
-class Home extends Component {
-  render() {
-      return (
-        <div className={'home'}>
-          <p>coming soon..</p>
-        </div>
-      )
-    }
-}
+const Home = () => (
+  <div className="home">
+    <p>coming soon..</p>
+  </div>
+);
 
 export default Home;
