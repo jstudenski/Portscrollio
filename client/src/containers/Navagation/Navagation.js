3efef444767f03d168d4aea1d566dfc1d896dc91
jstudenski <jstudenski@gmail.com>
2018-08-09 20:08:32 -0500

    lint

--- client/src/containers/Navagation/Navagation.js
+++ client/src/containers/Navagation/Navagation.js
@@ -23,16 +23,10 @@ class Navagation extends React.Component {
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
     window.addEventListener('resize', this.handleResize);
-
-    // console.log(this.state.links[0].text);
   }
 
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  getLinkMidpoint() {
-    //  console.log(this.state.links);
   }
 
   handleResize() {
@@ -59,15 +53,20 @@ class Navagation extends React.Component {
       // console.log("offsetTop", node.offsetTop);
       // console.log("scrollTop", node.scrollTop);
       // console.log("scrollTop", node.scrollTop);
+      //  console.log(link);
 
-      const mp = node.getBoundingClientRect().top + (node.getBoundingClientRect().height / 2) + window.scrollY;
+      const mp = node.getBoundingClientRect().top +
+                (node.getBoundingClientRect().height / 2) +
+                window.scrollY;
+
       link.midPoint = mp;
 
       const wh = this.state.windowHeight;
+
+      // Add class to change link color:
       switch (true) {
         case (mp > wh * 4):
           link.class = 'sec5 nav-link';
-          // link.text === 'Contact' ? link.class += 'active' : null;
           break;
         case (mp > wh * 3):
           link.class = 'sec4 nav-link';
@@ -82,15 +81,43 @@ class Navagation extends React.Component {
     }
   }
 
+  // calculateColor(link) {
+  //   console.log(link);
+  //   // console.log("HELLO WORLD");
+  //   switch (true) {
+  //     case (mp > wh * 4):
+  //     return 'salmon';
+  //       break;
+  //     case (mp > wh * 3):
+  //     return 'yellow';
+  //       break;
+  //     case (mp > wh * 2):
+  //     return 'red';
+  //       break;
+  //     default:
+  //     return 'lime';
+  //       break;
+  //   }
+
+    
+  // }
 
   render() {
     return (
       <nav style={{ position: this.state.scrollingLock ? 'fixed' : 'relative' }}>
         {this.props.children}
-        <div id="navbar" ref={this.navbar}>
+        <div
+          id="navbar"
+          ref={this.navbar}
+        >
           <ul>
             {this.state.links.map(link => (
-              <li ref={node => this.calculateMidpoint(node, link)} className={link.class} style={{ color: link.color }}>
+              <li
+                ref={node => this.calculateMidpoint(node, link)}
+                className={link.class}
+                //style={{ color: this.calculateColor(link) }}
+                // style={{ color: link.color }}
+              >
                 <ScrollTo selector={link.selector}>{link.text}</ScrollTo>
               </li>
             ))}
@@ -101,7 +128,6 @@ class Navagation extends React.Component {
   }
 }
 
-// onClick={this.changeContent.bind(this)}
 
 export default Navagation;
 
