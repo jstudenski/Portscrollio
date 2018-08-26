696e502df78b2f59126bdb810bdd9cf2420c10c5
Jeff Studenski <jstudenski@gmail.com>
2018-08-25 21:15:48 -0500

    simplify

--- client/src/containers/Navagation/NavItem/NavItem.js
+++ client/src/containers/Navagation/NavItem/NavItem.js
@@ -10,57 +10,106 @@ class NavItem extends React.Component {
       midpoint: 0,
       domRect: '',
     };
+    this.ref = React.createRef();
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
   //  console.log(this.state.midPoint);
+    // this.midpoint(this.myRef);
+  //  console.log(this.midpoint.current)
+    this.state.node = this.ref.current;
+    //this.textInput.current.focusTextInput();
   }
 
-  midpoint(node) {
-      if (node) {
-        this.setState({
+  // componentDidUpdate(){
+  //   console.log('updated');
+  // //  console.log(this.state.class);
+  //   this.handleScroll();
+  // }
 
-        });
-        const domRect = node.getBoundingClientRect();
-        this.state.midPoint = domRect.top + (domRect.height / 2) + window.scrollY;
 
-        switch (true) {
-          case (this.state.midPoint > this.props.windowHeight * 4):
-            //console.log('5');
-            this.state.class = '5';
-            break;
-          case (this.state.midPoint > this.props.windowHeight * 3):
-          //  console.log('4');
-            this.state.class = '4';
-            break;
-          case (this.state.midPoint > this.props.windowHeight * 2):
-          //  console.log('3');
-            this.state.class = '3';
-            break;
-          default:
-          //  console.log('2');
-            this.state.class = '2';
-            break;
-        }
+handleScroll(){
+    console.log('scroll');
+  // if(this.midpoint.current != undefined){
+    const node = this.ref.current;
+  //  console.log(node);
+    const domRect = node.getBoundingClientRect();
+    this.state.midPoint = domRect.top + (domRect.height / 2) + window.scrollY;
+  // }
 
-      }
+  switch (true) {
+    case (this.state.midPoint > this.props.windowHeight * 4):
+      //console.log('5');
+      this.state.class = '5';
+      break;
+    case (this.state.midPoint > this.props.windowHeight * 3):
+    //  console.log('4');
+      this.state.class = '4';
+      break;
+    case (this.state.midPoint > this.props.windowHeight * 2):
+    //  console.log('3');
+      this.state.class = '3';
+      break;
+    default:
+    //  console.log('2');
+      this.state.class = '2';
+      break;
   }
+
+}
+
+
+  //
+  // midpoint() {
+  //   const node = this.myRef.current;
+  //
+  //   console.log(node);
+  //
+  //     if (node) {
+  //       const domRect = node.getBoundingClientRect();
+  //
+  //       this.state.midPoint = domRect.top + (domRect.height / 2) + window.scrollY;
+  //
+  //       switch (true) {
+  //         case (this.state.midPoint > this.props.windowHeight * 4):
+  //           //console.log('5');
+  //           this.state.class = '5';
+  //           break;
+  //         case (this.state.midPoint > this.props.windowHeight * 3):
+  //         //  console.log('4');
+  //           this.state.class = '4';
+  //           break;
+  //         case (this.state.midPoint > this.props.windowHeight * 2):
+  //         //  console.log('3');
+  //           this.state.class = '3';
+  //           break;
+  //         default:
+  //         //  console.log('2');
+  //           this.state.class = '2';
+  //           break;
+  //       }
+  //
+  //     }
+  //
+  // }
 
 
   render() {
     return (
-      <div>
-          <li
-            ref={node => this.midpoint(node)}
 
+
+          <li
+            ref={this.ref}
+        //  ref={node => this.midpoint(node)}
             // style={{ color: link.color }}
           >
             <ScrollTo selector={this.props.selector}>
               {this.props.text}{this.state.class}
             </ScrollTo>
           </li>
-      </div>
+
     )
   }
 }
