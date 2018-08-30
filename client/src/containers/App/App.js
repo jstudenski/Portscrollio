cb92325ff5229ab9c50baa35625a452035a53bcd
Jeff Studenski <jstudenski@gmail.com>
2018-08-29 19:49:48 -0500

    map over array to create section components

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Section from '../Section';
 import Navagation from '../Navagation';
 import Home from '../Home';
@@ -10,26 +10,23 @@ import './App.css';
 class App extends Component {
   render() {
     const PAGES = [
-      { key: 0, selector: '#about', text: 'About', linkColor: 'blue' },
-      { key: 1, selector: '#skills', text: 'Skills', linkColor: 'red'},
-      { key: 2, selector: '#portfolio', text: 'Portfolio', linkColor: 'orange'},
-      { key: 3, selector: '#contact', text: 'Contact', linkColor: 'yellow'},
+      { key: 0, selector: '#about', text: 'About', linkColor: '#0b64c0', background: 'salmon', component: <About />},
+      { key: 1, selector: '#skills', text: 'Skills', linkColor: '#ff4552', background: '#efe8d8'},
+      { key: 2, selector: '#portfolio', text: 'Portfolio', linkColor: '#28292b', background: '#76eedf'},
+      { key: 3, selector: '#contact', text: 'Contact', linkColor: '#e5e8dc', background: '#ff8fa4', component: <Contact />},
     ]
-
+ // http://www.vanschneider.com/colors
     return (
       <div className="App">
         <Section class="section1" name="home">
           <Home />
         </Section>
         <Navagation pages={PAGES} />
-        <Section class="section2" name="about">
-          <About />
-       </Section>
-        <Section class="section3" name="skills" />
-        <Section class="section4" name="portfolio" />
-        <Section class="section5" name="contact">
-          <Contact />
-        </Section>
+        {PAGES.map(page => (
+          <Section background={page.background}>
+            {page.component}
+          </Section>
+        ))}
       </div>
     );
   }
