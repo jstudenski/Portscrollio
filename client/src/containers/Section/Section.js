12e0c71dede31830ffb9f614c8812dfc318933d7
Jeff Studenski <jstudenski@gmail.com>
2018-08-31 17:12:45 -0500

    moved grid container from compnent to section compoonent

--- client/src/containers/Section/Section.js
+++ client/src/containers/Section/Section.js
@@ -5,7 +5,7 @@ class Section extends Component {
   render() {
     return (
       <section style={{ backgroundColor: this.props.background }} id={this.props.id} className={this.props.class}>
-        <div className="content">
+        <div className="container">
           {this.props.children}
         </div>
       </section>
