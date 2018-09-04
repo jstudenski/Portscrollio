0e3ab39ea065721ad4b94da6829c9e590541585b
Jeff Studenski <jstudenski@gmail.com>
2018-09-04 14:58:41 -0500

    turned section into pure component

--- client/src/components/Section/Section.js
+++ client/src/components/Section/Section.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './Section.css';
 
-class Section extends Component {
-  render() {
-    return (
-      <section style={{ backgroundColor: this.props.background }} id={this.props.id} className={this.props.class}>
-        <div className="container">
-          {this.props.children}
-        </div>
-      </section>
-    );
-  }
+
+const Section = (props) => {
+  return (
+    <section
+      style={{
+        backgroundColor: props.background
+      }}
+      id={props.id}
+      className={props.class}
+    >
+      <div className="container">
+        {props.children}
+      </div>
+    </section>
+  );
 }
 
 export default Section;
